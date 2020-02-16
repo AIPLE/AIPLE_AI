ef518ad967bdd0d3ddcb5833e98b2d1cdae2145d
sangik-lee <dev_sang@naver.com>
2020-02-16 17:40:15 +0900

    go!

---
+++ API/inputpic.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2 as cv
+
+if __name__ == "__main__":
+    
+    pass
+
+class InputPic:
+    def setimg(self, inputimg):
+        img = cv.imread(inputimg, 0)
+        pass
+    
